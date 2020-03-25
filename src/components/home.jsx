c015d50ece4cdfcb5a0644011d0f93164aa3245e
Romain BLATRIX <romain.blatrix-ext@ouicar.fr>
2020-03-25 17:05:48 +0100

    add contact from home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -55,6 +55,43 @@ const HomeRecipeList = styled.div`
   }
 `;
 
+const ContactLink = styled.a`
+  padding: 10px 0;
+  width: 90px;
+  text-align: center;
+  border: 1px solid currentColor;
+  color: inherit;
+  text-decoration: none;
+  border-radius: 21px;
+  background-color: white;
+  transition: all 200ms ease;
+  position: relative;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-style: solid;
+    border-width: 20px 45px 0 45px;
+    border-color: lightgray transparent transparent transparent;
+    opacity: 0;
+  }
+  &:hover {
+    border-radius: 2px;
+    border-color: lightgray;
+    background-color: white;
+    transition: all 300ms ease;
+    & > span {
+      opacity: 0;
+      transition: opacity 100ms ease;
+    }
+    &::before {
+      opacity: 1;
+      transition: opacity 300ms ease 100ms;
+    }
+  }
+`;
+
 const Home = ({ className }) => {
   return (
     <Content>
@@ -69,6 +106,10 @@ const Home = ({ className }) => {
           <HomeRecipe id={id} key={id} title={title} />
         ))}
       </HomeRecipeList>
+      <H1>Contact</H1>
+      <ContactLink href="mailto:romain.blatrix@gmail.com?cc=chambriermatthieu@gmail.com&tommydessine@gmail.com">
+        <span>Par mail</span>
+      </ContactLink>
     </Content>
   );
 };
